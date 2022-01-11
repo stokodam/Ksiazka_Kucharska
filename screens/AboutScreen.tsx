44d925f1b6557b7591208e5b1a0831d0ab2f7512
stokodam <83658258+stokodam@users.noreply.github.com>
2022-01-11 15:14:48 +0100

    AboutScreen

--- screens/AboutScreen.tsx
+++ screens/AboutScreen.tsx
@@ -6,13 +6,20 @@ import { Text, View } from '../components/Themed';
 
 export default function ModalScreen() {
   return (
+  <View style={styles.container}>
     <View style={styles.container}>
-      <Text style={styles.title}>Modal</Text>
+      <Text style={styles.title}>Książka kucharska</Text>
+    </View>
+    <View style={styles.container}>
+      <Text style={styles.authors}>Authors:</Text>
+      <Text style={styles.authors}>Katarzyna Skrobisz</Text>
+      <Text style={styles.authors}>Jakub Pawlik</Text>
+      <Text style={styles.authors}>Daniel Stokowiec</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/ModalScreen.tsx" />
 
       {/* Use a light status bar on iOS to account for the black space above the modal */}
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
+    </View>
     </View>
   );
 }
@@ -21,12 +28,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
   },
   title: {
-    fontSize: 20,
+    marginTop: 10,
+    fontSize: 30,
     fontWeight: 'bold',
   },
+  authors: {
+      fontSize: 20,
+      bottom: 0,
+      fontWeight: 'bold',
+      justifyContent: 'center',
+    },
   separator: {
     marginVertical: 30,
     height: 1,
